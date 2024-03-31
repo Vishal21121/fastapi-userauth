1d0125a10fa28b59ede51c549a712c872559cff1
Vishal21121 <vs84077000@gmail.com>
2024-03-31 16:45:13 +0530

    added: requirements.txt and comments in the create_user function

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
 import bcrypt
-
-load_dotenv()
+import uvicorn
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client.fastapi_userauth
@@ -15,22 +13,30 @@ collection = db.users
 
 class User(BaseModel):
     username: str
-    email: str
+    email: EmailStr
     password: str
 
 app = FastAPI()
 
 @app.post("/api/v1/user/create")
-def home_page(user:User):
+def create_user(user:User):
     existing_user = collection.find_one({"email":user.email})
+    # check if user exist with the given email
     if(existing_user != None):
         return JSONResponse(status_code=404,content={"message":"Please enter another email address","success":False})
+    
+    # hash the provided password
     encoded_password = user.password.encode("utf-8")
     hashed_password = bcrypt.hashpw(encoded_password,bcrypt.gensalt(10))
+
+    # save the user in the database
     collection.insert_one({"username":user.username,"email":user.email,"password": hashed_password,"createdAt":datetime.now().strftime('%d-%m-%Y:%H:%M:%S')})
+
+    # check that the user is created successfully or not
     userFound = collection.find_one({"email":user.email})
     if(userFound==None):
         return JSONResponse(status_code=500,content={"message":"Internal server error","success":False})
+    
+    # if everything works fine
     return JSONResponse(status_code=201,content={"message":"user created successfully","success":True,"data":{"_id":str(userFound["_id"]),"email":userFound["email"],"username":userFound["username"]}})
-
 
