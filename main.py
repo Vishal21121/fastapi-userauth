ffda8341fab0c6240df7cffb2bdce9d0784363fe
Vishal21121 <vs84077000@gmail.com>
2024-03-31 17:01:00 +0530

    added: login_user function

--- main.py
+++ main.py
@@ -11,15 +11,19 @@ client = MongoClient(os.getenv("MONGO_URI"))
 db = client.fastapi_userauth
 collection = db.users
 
-class User(BaseModel):
+class CreateUser(BaseModel):
     username: str
+    email: EmailStr
+    password: str
+
+class LoginUser(BaseModel):
     email: EmailStr
     password: str
 
 app = FastAPI()
 
 @app.post("/api/v1/user/create")
-def create_user(user:User):
+def create_user(user:CreateUser):
     existing_user = collection.find_one({"email":user.email})
     # check if user exist with the given email
     if(existing_user != None):
@@ -40,3 +44,18 @@ def create_user(user:User):
     # if everything works fine
     return JSONResponse(status_code=201,content={"message":"user created successfully","success":True,"data":{"_id":str(userFound["_id"]),"email":userFound["email"],"username":userFound["username"]}})
 
+@app.post("/api/v1/user/login")
+def login_user(user:LoginUser):
+
+    # check if any user exists with the given email id
+    userFound = collection.find_one({"email":user.email})
+    if(userFound == None):
+        return JSONResponse(status_code=404,content={"message":"Please enter correct credentials","success":False})
+    
+    # compare password
+    isPasswordCorrect = bcrypt.checkpw(user.password.encode("utf-8"),userFound["password"])
+    if(isPasswordCorrect == False):
+       return JSONResponse(status_code=404,content={"message":"Please enter correct credentials","success":False})
+
+    # if everything works fine
+    return JSONResponse(status_code=200,content={"message":"loggedin successfully","success":True,"data":{"_id":str(userFound["_id"]),"email":userFound["email"],"username":userFound["username"]}})
